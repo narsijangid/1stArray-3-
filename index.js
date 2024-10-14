b561992ce2ad30efda84c4d90ba7a34112969b96
narsi-jangid <narsijangid01@gmail.com>
2024-10-14 22:19:31 +0530

    tasks

---
+++ index.js
@@ -0,0 +1,13 @@
+let tasks = ["running", "planning of website", "plan layout", "draw design", "deployment"];
+
+tasks.shift();
+
+tasks.unshift("make a layout", "make this design in 3d");
+
+
+
+tasks.pop();
+
+tasks.push("testing");
+
+console.log(tasks);
